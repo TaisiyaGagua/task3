d4bad12030de31fad433c21e616ec1319a358ed9
Таисия Гагуа <gagya@mail.ru>
2023-10-13 17:55:25 +0400

    Delete unnecessary file

--- comp.ts
+++
@@ -1,8 +0,0 @@
-import { createHmac } from "crypto";
-
-function computerMove(elements: string[]): string {
-    let randCompMoveIndex = Math.floor(Math.random() * elements.length);
-    return elements[randCompMoveIndex];
-}
-
-export { computerMove };
